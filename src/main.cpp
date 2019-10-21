98182604a38187921a35625839aec7424f1ff785
maxeisen <17mje@queensu.ca>
2019-10-21 10:04:58 -0400

    disabled framerate limit, added "sprites" folder

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 int main() {
     sf::RenderWindow window;
     window.create(sf::VideoMode::getDesktopMode(), "McLeod vs. The Crowd", sf::Style::None);
-    window.setFramerateLimit(60);
+    // window.setFramerateLimit(60);
     // Enable vertical sync. (vsync)
     window.setVerticalSyncEnabled(true);
     // When a key is pressed, sf::Event::KeyPressed will be true only once
