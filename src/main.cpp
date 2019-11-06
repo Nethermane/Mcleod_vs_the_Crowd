cc9e745269b184d7b65a064ba55233fb60475ed3
Bruce Nishimura <16ban1@caslab.queensu.ca>
2019-11-06 14:17:44 -0500

    Cap frames, add anti-aliasing

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,9 @@
 
 int main() {
     sf::RenderWindow window;
-    window.create(sf::VideoMode::getDesktopMode(), "McLeod vs. The Crowd", sf::Style::None);
+    window.create(sf::VideoMode::getDesktopMode(), "McLeod vs. The Crowd", sf::Style::None, sf::ContextSettings(0,0,16));
     window.setKeyRepeatEnabled(false);
-    window.setFramerateLimit(0);
+    window.setFramerateLimit(200);
     GameManager game(window);
     return 0;
 }
